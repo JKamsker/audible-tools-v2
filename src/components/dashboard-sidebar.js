7e7bc4a663d68c058d24c6e64ec482fb10686f22
Jonas Kamsker <git.jonas@kamsker.at>
2022-04-05 00:47:01 +0200

    Meh

--- src/components/dashboard-sidebar.js
+++ src/components/dashboard-sidebar.js
@@ -20,43 +20,43 @@ const items = [
   {
     href: '/',
     icon: (<ChartBarIcon fontSize="small" />),
-    title: 'Dashboard'
+    title: 'Conversion'
   },
   {
     href: '/customers',
-    icon: (<UsersIcon fontSize="small" />),
-    title: 'Customers'
-  },
-  {
-    href: '/products',
     icon: (<ShoppingBagIcon fontSize="small" />),
-    title: 'Products'
+    title: 'Lookup Checksum'
   },
-  {
-    href: '/account',
-    icon: (<UserIcon fontSize="small" />),
-    title: 'Account'
-  },
-  {
-    href: '/settings',
-    icon: (<CogIcon fontSize="small" />),
-    title: 'Settings'
-  },
-  {
-    href: '/login',
-    icon: (<LockIcon fontSize="small" />),
-    title: 'Login'
-  },
-  {
-    href: '/register',
-    icon: (<UserAddIcon fontSize="small" />),
-    title: 'Register'
-  },
-  {
-    href: '/404',
-    icon: (<XCircleIcon fontSize="small" />),
-    title: 'Error'
-  }
+  // {
+  //   href: '/products',
+  //   icon: (<ShoppingBagIcon fontSize="small" />),
+  //   title: 'Info'
+  // },
+  // {
+  //   href: '/account',
+  //   icon: (<UserIcon fontSize="small" />),
+  //   title: 'Account'
+  // },
+  // {
+  //   href: '/settings',
+  //   icon: (<CogIcon fontSize="small" />),
+  //   title: 'Settings'
+  // },
+  // {
+  //   href: '/login',
+  //   icon: (<LockIcon fontSize="small" />),
+  //   title: 'Login'
+  // },
+  // {
+  //   href: '/register',
+  //   icon: (<UserAddIcon fontSize="small" />),
+  //   title: 'Register'
+  // },
+  // {
+  //   href: '/404',
+  //   icon: (<XCircleIcon fontSize="small" />),
+  //   title: 'Error'
+  // }
 ];
 
 export const DashboardSidebar = (props) => {
@@ -91,7 +91,7 @@ export const DashboardSidebar = (props) => {
         }}
       >
         <div>
-          <Box sx={{ p: 3 }}>
+          <Box sx={{ padding: 2 }}>
             <NextLink
               href="/"
               passHref
@@ -106,49 +106,11 @@ export const DashboardSidebar = (props) => {
               </a>
             </NextLink>
           </Box>
-          <Box sx={{ px: 2 }}>
-            <Box
-              sx={{
-                alignItems: 'center',
-                backgroundColor: 'rgba(255, 255, 255, 0.04)',
-                cursor: 'pointer',
-                display: 'flex',
-                justifyContent: 'space-between',
-                px: 3,
-                py: '11px',
-                borderRadius: 1
-              }}
-            >
-              <div>
-                <Typography
-                  color="inherit"
-                  variant="subtitle1"
-                >
-                  Acme Inc
-                </Typography>
-                <Typography
-                  color="neutral.400"
-                  variant="body2"
-                >
-                  Your tier
-                  {' '}
-                  : Premium
-                </Typography>
-              </div>
-              <SelectorIcon
-                sx={{
-                  color: 'neutral.500',
-                  width: 14,
-                  height: 14
-                }}
-              />
-            </Box>
-          </Box>
         </div>
         <Divider
           sx={{
             borderColor: '#2D3748',
-            my: 3
+            marginBottom: 3
           }}
         />
         <Box sx={{ flexGrow: 1 }}>
@@ -160,57 +122,6 @@ export const DashboardSidebar = (props) => {
               title={item.title}
             />
           ))}
-        </Box>
-        <Divider sx={{ borderColor: '#2D3748' }} />
-        <Box
-          sx={{
-            px: 2,
-            py: 3
-          }}
-        >
-          <Typography
-            color="neutral.100"
-            variant="subtitle2"
-          >
-            Need more features?
-          </Typography>
-          <Typography
-            color="neutral.500"
-            variant="body2"
-          >
-            Check out our Pro solution template.
-          </Typography>
-          <Box
-            sx={{
-              display: 'flex',
-              mt: 2,
-              mx: 'auto',
-              width: '160px',
-              '& img': {
-                width: '100%'
-              }
-            }}
-          >
-            <img
-              alt="Go to pro"
-              src="/static/images/sidebar_pro.png"
-            />
-          </Box>
-          <NextLink
-            href="https://material-kit-pro-react.devias.io/"
-            passHref
-          >
-            <Button
-              color="secondary"
-              component="a"
-              endIcon={(<OpenInNewIcon />)}
-              fullWidth
-              sx={{ mt: 2 }}
-              variant="contained"
-            >
-              Pro Live Preview
-            </Button>
-          </NextLink>
         </Box>
       </Box>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,132 @@
 import Head from 'next/head';
 import { Box, Container, Grid } from '@mui/material';
 import { Budget } from '../components/dashboard/budget';
-import { LatestOrders } from '../components/dashboard/latest-orders';
-import { LatestProducts } from '../components/dashboard/latest-products';
-import { Sales } from '../components/dashboard/sales';
-import { TasksProgress } from '../components/dashboard/tasks-progress';
-import { TotalCustomers } from '../components/dashboard/total-customers';
-import { TotalProfit } from '../components/dashboard/total-profit';
-import { TrafficByDevice } from '../components/dashboard/traffic-by-device';
+// import { LatestOrders } from '../components/dashboard/latest-orders';
+// import { LatestProducts } from '../components/dashboard/latest-products';
+// import { Sales } from '../components/dashboard/sales';
+// import { TasksProgress } from '../components/dashboard/tasks-progress';
+// import { TotalCustomers } from '../components/dashboard/total-customers';
+// import { TotalProfit } from '../components/dashboard/total-profit';
+// import { TrafficByDevice } from '../components/dashboard/traffic-by-device';
 import { DashboardLayout } from '../components/dashboard-layout';
+
+import React from 'react'
+import Dropzone from 'react-dropzone'
+import { useDropzone } from 'react-dropzone';
+
+// import { Avatar, Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent } from '@mui/material';
+// import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+// import MoneyIcon from '@mui/icons-material/Money';
+
+// import { useDropzone } from 'react-dropzone';
+import styled from 'styled-components';
+
+
+const RenderContainer = () => {
+  return (<Grid
+    item
+    lg={3}
+    sm={6}
+    xl={3}
+    xs={12}
+  >
+    <Budget />
+  </Grid>);
+};
+
+// const getColor = (props) => {
+//   if (props.isDragAccept) {
+//     return '#00e676';
+//   }
+//   if (props.isDragReject) {
+//     return '#ff1744';
+//   }
+//   if (props.isFocused) {
+//     return '#2196f3';
+//   }
+//   return '#eeeeee';
+// }
+
+// const DummyDropZone = styled.div`
+//   padding: 30px;
+//   margin-left: 10px; 
+//   margin-right: -10px;
+//   margin-bottom: -15px;
+//   border-width: 2px;
+//   border-radius: 2px;
+//   border-color: ${props => getColor(props)};
+//   border-style: dashed;
+//   background-color: #fafafa;
+//   color: #bdbdbd;
+//   outline: none;
+//   transition: border .24s ease-in-out;
+//   width: 100%;
+//   text-align: center;
+// `;
+
+// const RealDropZone = styled.div`
+//   box-sizing: border-box;
+//   display: none;
+//   position: fixed;
+//   width: 100%;
+//   height: 100%;
+//   left: 0;
+//   top: 0;
+//   z-index: 99999;
+
+//   background: rgba(#60a7dc,.8);
+//   border: 11px dashed #60a7dc;
+// `;
+
+
+// function StyledDropzone(props) {
+//   const {
+//     getRootProps,
+//     getInputProps,
+//     isFocused,
+//     isDragAccept,
+//     isDragReject
+//   } = useDropzone({accept: 'image/*'});
+
+//   return (
+//      <DummyDropZone {...getRootProps({isFocused, isDragAccept, isDragReject})}>
+//         <input {...getInputProps()} />
+//         <p>Drag 'n' drop some files here, or click to select fil es</p>
+//       </DummyDropZone>
+//   );
+// }
+
+
+const RenderDropZone = () => {
+  return (<Grid
+    item
+    // lg={3}
+    // sm={6}
+    // xl={3}
+    // xs={{gridColumn: '12', gridRow: 'span 12'}}
+    xs={12}
+  // minHeight="100px"
+  >
+    <Card
+      sx={{ height: '100%' }}
+    >
+      <CardContent>
+        <Grid
+          container
+          spacing={3}
+        // sx={{ justifyContent: 'space-between' }}
+        // xs={12}
+        >
+          <StyledDropzone />
+
+        </Grid>
+
+      </CardContent>
+    </Card>
+
+  </Grid>);
+};
 
 const Dashboard = () => (
   <>
@@ -17,93 +135,43 @@ const Dashboard = () => (
         Dashboard | Material Kit
       </title>
     </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8
-      }}
-    >
-      <Container maxWidth={false}>
-        <Grid
-          container
-          spacing={3}
-        >
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
-            <Budget />
-          </Grid>
-          <Grid
-            item
-            xl={3}
-            lg={3}
-            sm={6}
-            xs={12}
-          >
-            <TotalCustomers />
-          </Grid>
-          <Grid
-            item
-            xl={3}
-            lg={3}
-            sm={6}
-            xs={12}
-          >
-            <TasksProgress />
-          </Grid>
-          <Grid
-            item
-            xl={3}
-            lg={3}
-            sm={6}
-            xs={12}
-          >
-            <TotalProfit sx={{ height: '100%' }} />
-          </Grid>
-          <Grid
-            item
-            lg={8}
-            md={12}
-            xl={9}
-            xs={12}
-          >
-            <Sales />
-          </Grid>
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xl={3}
-            xs={12}
-          >
-            <TrafficByDevice sx={{ height: '100%' }} />
-          </Grid>
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xl={3}
-            xs={12}
-          >
-            <LatestProducts sx={{ height: '100%' }} />
-          </Grid>
-          <Grid
-            item
-            lg={8}
-            md={12}
-            xl={9}
-            xs={12}
-          >
-            <LatestOrders />
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
+
+    <Dropzone
+      noClick
+      onDrop={acceptedFiles => {
+        console.log(acceptedFiles);
+        // this.acceptFiles(acceptedFiles);
+      }}>
+      {({ getRootProps, getInputProps }) => (
+        <section>
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <Box
+              component="main"
+              sx={{
+                flexGrow: 1,
+                py: 8
+              }}
+              paddingBottom={200}
+            >
+              <Container maxWidth={false}>
+                <Grid
+                  container
+                  spacing={3}
+                >
+                  {RenderContainer()}
+                  {RenderContainer()}
+                  {/* {RenderDropZone()} */}
+                </Grid>
+              </Container>
+            </Box>
+
+          </div>
+        </section>
+      )}
+    </Dropzone>
+
+
   </>
 );
 
